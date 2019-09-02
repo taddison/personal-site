71649891ea905869257ffc8831bc3db41818b099
Tim Addison <git@tjaddison.com>
2019-09-01 20:06:06 -0400

    Remove extra brace

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,55 +9,49 @@ const BlogPostTemplate = props => {
   const { previous, next } = props.pageContext
 
   return (
-      <Layout>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <h1
-          className="text-4xl font-bold"
-        >
-          {post.frontmatter.title}
-        </h1>
-        <p
-          className="mb-2 italic text-gray-500"
-        >
-          {post.frontmatter.date}
-        </p>
-        <div className="markdown" dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: 1,
-          }}
-        />
+    <Layout>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <h1 className="text-4xl font-bold">{post.frontmatter.title}</h1>
+      <p className="mb-2 italic text-gray-500">{post.frontmatter.date}</p>
+      <div
+        className="markdown"
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+      <hr
+        style={{
+          marginBottom: 1,
+        }}
+      />
 
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    )
-  }
+      <ul
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+    </Layout>
+  )
 }
 
 export default BlogPostTemplate
