44b9b104c7082ab972cdb3b1f675f5b9f40f6fea
Tim Addison <git@tjaddison.com>
2019-09-07 16:30:32 -0400

    Tag page with posts

--- src/components/TagPill.jsx
+++ src/components/TagPill.jsx
@@ -8,7 +8,7 @@ const propTypes = {
 
 const TagPill = ({ tag }) => {
   return (
-    <Link to={`/blog/tag/${tag}`}>
+    <Link to={`/blog/tags/#${tag}`}>
       <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 border-accent-5 border block hover:bg-accent-3 hover:text-white hover:border-accent-3">
         #{tag}
       </span>
--- src/pages/blog/tags.js
+++ src/pages/blog/tags.js
@@ -1,14 +1,62 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
 const TagIndex = props => {
+  const { tagGroup } = useStaticQuery(
+    graphql`
+      query {
+        tagGroup: allMarkdownRemark {
+          group(field: frontmatter___tags) {
+            tag: fieldValue
+            totalCount
+            posts: nodes {
+              frontmatter {
+                date(formatString: "MMMM YYYY")
+                title
+              }
+              fields {
+                slug
+              }
+              id
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const tagGroups = tagGroup.group
+
   return (
     <Layout>
       <SEO title="Post tags" />
-      <div>tags</div>
+      <h2 className="text-3xl font-bold mb-4">Posts by Tag</h2>
+      {tagGroups.map(group => {
+        return (
+          <div key={group.tag} id={group.tag} className="mb-4">
+            <h2 className="font-semibold text-xl">
+              {group.tag}
+            </h2>
+            <div className="italic text-gray-500 text-sm">
+              {group.totalCount} post{group.totalCount === 1 ? "" : "s"}
+            </div>
+            {group.posts.map(post => {
+              return (
+                <div><Link key={post.id} to={post.fields.slug} className="hover:text-accent-3">
+                  {post.frontmatter.title}{" "}
+                </Link>
+                  <span className="text-gray-500 text-sm">
+                    {post.frontmatter.date}
+                  </span>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
     </Layout>
   )
 }
