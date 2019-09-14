c734489e0927df6ae9ed927fd17212a41489925c
Tim Addison <git@tjaddison.com>
2019-09-14 15:00:45 -0400

    add button on small

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,19 @@ const Header = ({ title, hideAbout = false }) => {
           {title}
         </Link>
       </h1>
-      <div>
+      <div className="block lg:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-accent-5 border-accent-5 hover:text-accent-3 hover:border-accent-3">
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div className="hidden lg:block">
         {!hideAbout && (
           <>
             <Link to="/about" className="font-semibold hover:text-accent-3">
