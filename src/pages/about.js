abffa5f699d681aa021fee66569e2004e269cc55
Tim Addison <git@tjaddison.com>
2019-09-21 21:48:29 -0400

    less chilli more shark

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
-import chilli from "../../content/assets/chilli.jpg"
+import shark from "../../content/assets/shark.jpg"
+import azores from "../../content/assets/azores.jpg"
 import Helmet from "react-helmet"
 import SEO from "../components/seo"
 import Header from "../components/Header"
@@ -17,7 +18,7 @@ const About = () => {
         <style type="text/css">
           {`
           body {
-            background-image: url('https://source.unsplash.com/1L71sPT5XKc')
+            background-image: url('${azores}')
           }
         `}
         </style>
@@ -33,7 +34,7 @@ const About = () => {
           <div className="p-4 md:p-12 text-center lg:text-left">
             <div
               className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
-              style={{ backgroundImage: "url(" + chilli + ")" }}
+              style={{ backgroundImage: "url(" + shark + ")" }}
             ></div>
 
             <h1 className="text-3xl font-bold pt-8 lg:pt-0">Tim Addison</h1>
@@ -62,7 +63,7 @@ const About = () => {
               Building and empowering engineering teams. Humbled to work with a
               fantastic group of engineers. Suffers occasional flashbacks to
               time in the DBA trenches (AGs and IMOLTP? I've got some war
-              stories...). Quite likes chilli.
+              stories...). Would probably lose a fight with a shark.
             </p>
 
             <div className="pt-12 pb-8">
@@ -97,15 +98,25 @@ const About = () => {
                   <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
                 </svg>
               </a>
+              <a className="ml-2" href="https://linkedin.com/in/tjaddison">
+                <svg
+                  className="h-6 fill-current text-gray-600 hover:text-teal-700"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <title>LinkedIn</title>
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                </svg>
+              </a>
             </div>
           </div>
         </div>
 
         <div className="w-full lg:w-2/5">
           <img
-            src={chilli}
+            src={shark}
             className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"
-            alt="Tim with a bunch of chilli"
+            alt="Tim with a shark"
           />
         </div>
       </div>
