67e050b3bf0e9affaac236885c885a7719dbb979
Tim Addison <git@tjaddison.com>
2019-09-22 16:11:39 -0400

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,24 @@ class Home extends React.Component {
     return (
       <Layout location={this.props.location}>
         <SEO title="All posts" />
-        <p>
-          Hey. Read the <Link to="/blog">blog.</Link>
-        </p>
+        <div className="text-xl">
+          <span className="block text-4xl mb-4 font-bold">Hey there! 👋</span>
+          Check out my{" "}
+          <Link
+            className="font-semibold hover:underline hover:text-accent-3"
+            to="/blog"
+          >
+            technical blog
+          </Link>
+          , or read a little more{" "}
+          <Link
+            className="font-semibold hover:underline hover:text-accent-3"
+            to="/about"
+          >
+            about me
+          </Link>
+          .<span className="block italic ml-4 mt-4">- Tim</span>
+        </div>
       </Layout>
     )
   }
