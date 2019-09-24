3a3397a11cfb0cf57b57d7261015078d49289826
Tim Addison <git@tjaddison.com>
2019-09-23 21:09:33 -0400

    remove unused graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,10 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Header from "./header"
 
 const Layout = props => {
   const { children } = props
-
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            author
-            social {
-              twitter
-              github
-              linkedin
-            }
-          }
-        }
-      }
-    `
-  )
-
-  const { author, title, social } = site.siteMetadata
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -32,35 +12,21 @@ const Layout = props => {
       <div className="mx-auto max-w-5xl px-8 md:px-0 flex-1 w-full md:w-5/6">
         <main>{children}</main>
       </div>
-      <footer className="bg-accent-1 border-accent-5 border-t pb-10 pt-6 mt-6 flex flex-col items-center">
-        <div className="mb-4 font-semibold">
-          <a
-            className="hover:text-accent-3"
-            href={`https://www.github.com/${social.github}`}
-          >
-            GitHub
-          </a>
-          {` `}
-          <a
-            className="hover:text-accent-3"
-            href={`https://twitter.com/${social.twitter}`}
-          >
-            Twitter
-          </a>
-          {` `}
-          <a
-            className="hover:text-accent-3"
-            href={`https://www.linkedin.com/in/${social.linkedin}`}
-          >
-            LinkedIn
-          </a>
-          {` `}
-          <a className="hover:text-accent-3" href={`/rss.xml`}>
-            RSS
-          </a>
-        </div>
+      <footer className="bg-accent-1 border-accent-5 border-t py-6 mt-6 flex flex-col items-center">
         <div>
-          {author} • {new Date().getFullYear()} • tjaddison.com
+          <Link
+            className="font-semibold hover:text-accent-3 hover:underline"
+            to="/about"
+          >
+            Tim Addison
+          </Link>{" "}
+          • {new Date().getFullYear()} •{" "}
+          <Link
+            className="font-semibold hover:text-accent-3 hover:underline"
+            to="/"
+          >
+            tjaddison.com
+          </Link>
         </div>
       </footer>
     </div>
