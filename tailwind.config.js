b6b4338aa2893e94f6fc10652e951b7d6c5dcc29
Tim Addison <git@tjaddison.com>
2020-09-13 21:06:04 -0400

    replace gatsby-purgecss with native tailwindcss purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: false,
+  purge: [`./src/**/*.js`],
   theme: {
     extend: {
       opacity: {
