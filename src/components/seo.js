a13bc5f5804bf3727a8cfef7e235a65e96a43cde
Tim Addison <git@tjaddison.com>
2020-11-02 19:31:34 -0500

    self-host inter

--- src/components/seo.js
+++ src/components/seo.js
@@ -72,10 +72,6 @@ function SEO({ description, lang, meta, title, image }) {
       ></link>
       <link rel="webmention" href={siteMetadata.social.webmention} />
       <link rel="pingback" href={siteMetadata.social.pingback} />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet"
-      />
     </Helmet>
   )
 }
